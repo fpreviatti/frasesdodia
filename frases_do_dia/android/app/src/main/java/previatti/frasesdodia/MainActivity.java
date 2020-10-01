19470aa0aaac3cf80cdcf20a52e6ce26ec28da06
Fábio Previatti <fpreviatti@hotmail.com>
2020-10-01 20:12:41 -0300

    first commit

---
+++ frases_do_dia/android/app/src/main/java/previatti/frasesdodia/MainActivity.java
@@ -0,0 +1,6 @@
+package previatti.frasesdodia;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ frases_do_dia/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main (){
+
+
+  runApp(MaterialApp(
+    title: "Frases do dia",
+    home: Container(color: Colors.white,),
+
+  ));
+
+  imprime();
+
+
+}
+
+void imprime(){
+
+
+}
+
