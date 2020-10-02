5422879fe590db121951bb301b5afc5746d457fc
Fábio Previatti <fpreviatti@hotmail.com>
2020-10-01 21:45:28 -0300

    formatacao de textos

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main (){
@@ -5,12 +7,35 @@ void main (){
 
   runApp(MaterialApp(
     title: "Frases do dia",
-    home: Container(color: Colors.white,),
 
+    home: Container(
+        color: Colors.white,
+        child: Column(
+
+          children: [
+            Text(
+                "Lorem ipsum",
+              style: TextStyle(
+                fontSize: 35,
+                fontStyle: FontStyle.normal,
+                fontWeight: FontWeight.normal,
+                color: Colors.black,
+                letterSpacing: 0,
+                wordSpacing: 0,
+                decoration: TextDecoration.underline,
+                decorationColor: Colors.greenAccent,
+                decorationStyle: TextDecorationStyle.wavy,
+
+
+
+              ),
+            ),
+          ],
+    ) ,
+    ),
   ));
 
   imprime();
-
 
 }
 
